8a3d37c4269efd96bd4843cb191005c6045c0edf
Jeff Jagger <jaggerjeffrey@gmail.com>
2018-03-25 10:57:06 -0400

    added tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318220901) do
+ActiveRecord::Schema.define(version: 20180325145554) do
+
+  create_table "tokens", force: :cascade do |t|
+    t.string "email"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
